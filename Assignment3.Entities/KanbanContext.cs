b4c46a8d4a45f73f30f8b16544e4f558c4bb872a
thekure <laku@itu.dk>
2022-09-27 16:00:14 +0200

    kures crap

--- Assignment3.Entities/KanbanContext.cs
+++ Assignment3.Entities/KanbanContext.cs
@@ -1,5 +1,31 @@
 namespace Assignment3.Entities;
 
-public class KanbanContext
+public sealed class KanbanContext : DbContext
 {
+    public DbSet<Task> Tasks => Set<Task>();
+    public DbSet<Tag> Tags => Set<Tag>();
+    public DbSet<User> Users => Set<User>();
+
+    public KanbanContext(DbContextOptions<KanbanContext> options)
+        : base(options){
+            
+        }
+
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder
+        .Entity<Task>()
+        .Property(e => e.State)
+        .HasConversion(
+            v => v.ToString(),
+            v => (State)Enum.Parse(typeof(State), v));
+
+        modelBuilder
+        .Entity<Task>()
+        .HasMany<Tag>(e => e.Tags)
+        .WithMany(e => e.Tasks);
+
+    }
 }
+
--- Assignment3.Entities/TagRepository.cs
+++ Assignment3.Entities/TagRepository.cs
@@ -1,5 +1,98 @@
+using Assignment3.Core;
 namespace Assignment3.Entities;
 
-public class TagRepository
+public class TagRepository : ITagRepository
 {
-}
+    private readonly KanbanContext _context;
+
+    public TagRepository(KanbanContext context){
+        _context = context;
+    }
+
+    public (Response Response, int TagId) Create(TagCreateDTO tag)
+    {
+        var entity = _context.Tags.FirstOrDefault(t => t.Name == tag.Name);
+        Response Response;
+
+        if (entity is null){
+            entity = new Tag(){Name = tag.Name};
+
+            _context.Tags.Add(entity);
+            _context.SaveChanges();
+
+            Response = Response.Created;
+        } else {
+            Response = Response.Conflict;
+        }
+
+        return (Response, entity.Id);
+        
+    }
+
+    public Response Delete(int tagId, bool force = false)
+    {
+        var tag = _context.Tags.Include(t => t.Tasks).FirstOrDefault(t => t.Id == tagId);
+        Response response;
+
+        if (tag is null)
+        {
+            response = Response.NotFound;
+        }
+        else if (tag.Tasks.Any() && !force)
+        {
+            response = Response.Conflict;
+        }
+        else
+        {
+            _context.Tags.Remove(tag);
+            _context.SaveChanges();
+
+            response = Response.Deleted;
+        }
+
+        return response;
+    }
+
+    public TagDTO Read(int tagId)
+    {
+        var tags = from t in _context.Tags
+                     where t.Id == tagId
+                     select new TagDTO(t.Id, t.Name);
+
+        return tags.FirstOrDefault();
+    }
+
+    public IReadOnlyCollection<TagDTO> ReadAll()
+    {
+        var tags =  from t in _context.Tags
+                    orderby t.Name
+                    select new TagDTO(t.Id, t.Name);
+
+        return tags.ToArray();
+    }
+
+    public Response Update(TagUpdateDTO tag)
+    {
+        var entity = _context.Tags.Include(t => t.Tasks).FirstOrDefault(t => t.Id == tag.Id);
+        Response response;
+
+        if (entity is null)
+        {
+            response = Response.NotFound;
+        }
+        else if (_context.Tags.FirstOrDefault(t => t.Id != tag.Id && t.Name == tag.Name) != null)
+        {
+            response = Response.Conflict;
+        }
+        else
+        {
+            entity.Name = tag.Name;
+            _context.SaveChanges();
+
+            response = Response.Updated;
+        }
+
+        return response;
+        }
+    }
+

f81c97e3d820f6cb51484c64d4ecf158d25033f7
thekure <laku@itu.dk>
2022-09-29 15:20:27 +0200

    Some changes

--- Assignment3.Entities.Tests/TaskRepositoryTests.cs
+++ Assignment3.Entities.Tests/TaskRepositoryTests.cs
@@ -22,15 +22,95 @@ private readonly KanbanContext _context;
     }
 
 
-    // [Fact]
-    // public void getAssignedUserName_Should()
-    // {
-    //     var (response, created) = _repository.Create(new TaskCreateDTO("taskTitle", null, null, new List<string>()));
-       
-    //     response.Should().Be(Response.Created);
-       
-    //     created.Should().Be(0);
-    // }
+    [Fact]
+    public void Create_Should_Return_Created_and_0()
+    {
+        // Arrange
+        var tasksListOfTags = new List<string>{"testTag"};
+        var (userResponse, userId) = _userRepository.Create(new UserCreateDTO("HansGruber", "hansgruber@gmail.com"));
+        // var (tagResponse, tagId) = _tagRepository.Create(new TagCreateDTO("testTag"));
+        // var (taskResponse, taskId) = _taskRepository.Create(new TaskCreateDTO ("testTask", 0, null, tasksListOfTags));
+
+        // Act
+        var (testResponse, testId) = _taskRepository.Create(new TaskCreateDTO("taskTitle", 0, null, new List<string>()));
+
+        // Assert
+        testResponse.Should().Be(Response.Created);
+        testId.Should().Be(1);
+    }
+
+    [Fact]
+    public void Create_Should_Return_Conflict()
+    {
+        // Arrange
+        var tasksListOfTags = new List<string>{"testTag"};
+        var (userResponse, userId) = _userRepository.Create(new UserCreateDTO("HansGruber", "hansgruber@gmail.com"));
+        // var (tagResponse, tagId) = _tagRepository.Create(new TagCreateDTO("testTag"));
+        // var (taskResponse, taskId) = _taskRepository.Create(new TaskCreateDTO ("testTask", 0, null, tasksListOfTags));
+
+        // Act
+        var (testResponse0, testId0) = _taskRepository.Create(new TaskCreateDTO("taskTitle", 0, null, new List<string>()));
+        var (testResponse, testId) = _taskRepository.Create(new TaskCreateDTO("taskTitle", 0, null, new List<string>()));
+
+        // Assert
+        testResponse.Should().Be(Response.Conflict);
+    }
+
+    [Fact]
+    public void Delete_Should_Return_Deleted()
+    {
+        // Arrange 
+        var tasksListOfTags = new List<string>{"testTag"};
+        var (userResponse, userId) = _userRepository.Create(new UserCreateDTO("HansGruber", "hansgruber@gmail.com"));
+        // var (tagResponse, tagId) = _tagRepository.Create(new TagCreateDTO("testTag"));
+        var (taskResponse, taskId) = _taskRepository.Create(new TaskCreateDTO ("testTask", 0, null, tasksListOfTags));
+
+        // Act
+        var updateResponse = _taskRepository.Update(new TaskUpdateDTO(1, "testTask", 0, null, tasksListOfTags, State.Active));
+        var testResponse = _taskRepository.Delete(1);
+
+        // Assert
+        testResponse.Should().Be(Response.Deleted);
+    }
+
+    [Fact]
+    public void Delete_Should_Return_NotFound()
+    {
+        // Arrange 
+        var tasksListOfTags = new List<string>{"testTag"};
+        var (userResponse, userId) = _userRepository.Create(new UserCreateDTO("HansGruber", "hansgruber@gmail.com"));
+        // var (tagResponse, tagId) = _tagRepository.Create(new TagCreateDTO("testTag"));
+        // var (taskResponse, taskId) = _taskRepository.Create(new TaskCreateDTO ("testTask", 0, null, tasksListOfTags));
+
+        // Act
+        var testResponse = _taskRepository.Delete(1);
+
+        // Assert
+        testResponse.Should().Be(Response.NotFound);
+    }
+
+
+
+    [Fact]
+    public void ReadAllRemoved_Should_Return_testTask()
+    {
+        // Arrange 
+        var tasksListOfTags = new List<string>{"testTag"};
+        var removedTasks = new List<TaskDTO>{new TaskDTO(1, "testTask", "HansGruber", tasksListOfTags, State.Removed)};
+        var (userResponse, userId) = _userRepository.Create(new UserCreateDTO("HansGruber", "hansgruber@gmail.com"));
+        // var (tagResponse, tagId) = _tagRepository.Create(new TagCreateDTO("testTag"));
+        var (taskResponse, taskId) = _taskRepository.Create(new TaskCreateDTO ("testTask", 0, null, tasksListOfTags));
+
+        // Act
+        var updateResponse = _taskRepository.Update(new TaskUpdateDTO(1, "testTask", 0, null, tasksListOfTags, State.Active));
+        var testResponse = _taskRepository.Delete(1);
+        var testRemovedTasks = _taskRepository.ReadAllRemoved();
+
+        // Assert
+        testRemovedTasks.Should().BeEquivalentTo(removedTasks);
+    }
+
+ 
 
     
 
